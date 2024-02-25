901d01ed056c16e0ec2185d468e14068b9d55c9b
justin-aquino <justin.aquino25@gmail.com>
2024-02-24 16:54:17 -0800

    authentication start

--- app/(auth)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const RegisterPage = () => {
- return <div>RegisterPage</div>;
-};
-
-export default RegisterPage;
-
-//by using the parentheses on the auth folder, it tells the router to not the name of the folder as part of the URL.
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
-      <Button variant="destructive">Destructive</Button>
+      <p>Only authenticated users can see this</p>
     </>
   );
 }
