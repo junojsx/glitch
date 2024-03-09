cd26a0acd6237af7b4eea92d7e548cfe4f3e98ef
justin-aquino <justin.aquino25@gmail.com>
2024-03-08 23:28:39 -0800

    changed glitch font and logo color contrast

--- app/(auth)/_components/logo.tsx
+++ app/(auth)/_components/logo.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
-import { Poppins } from "next/font/google";
+import { Press_Start_2P } from "next/font/google";
 
 import { cn } from "@/lib/utils";
 
-const font = Poppins({
+const font = Press_Start_2P({
  subsets: ["latin"],
- weight: ["200", "300", "400", "500", "600", "700", "800"],
+ weight: "400",
 });
 
 export const Logo = () => {
  return (
   <>
    <div className="flex flex-col items-center gap-y-4">
-    <div className="bg-white rounded-full p-1">
-     <Image src="/spooky.png" alt="Glitch-logo" width="80" height="80" />
+    <div className="bg-gray-50 rounded-md p-2">
+     <Image src="/spooky.png" alt="Glitch-logo" width="100" height="100" />
     </div>
     <div className="flex flex-col items-center">
      <h1 className={cn("text-xl font-semibold", font.className)}>Glitch</h1>
