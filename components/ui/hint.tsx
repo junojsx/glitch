32086aab07ed4985eaadec79443a5c52c07ebfd0
justin-aquino <justin.aquino25@gmail.com>
2024-03-11 22:48:54 -0700

    initializing tooltip component

---
+++ components/ui/hint.tsx
@@ -0,0 +1,26 @@
+import {
+ Tooltip,
+ TooltipContent,
+ TooltipProvider,
+ TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+interface HintProps {
+ label: string;
+ children: React.ReactNode;
+ asChild?: boolean;
+ side?: "top" | "bottom" | "left" | "right";
+ align?: "start" | "center" | "end";
+}
+
+const Hint = ({ label, children, asChild, side, align }: HintProps) => {
+ return (
+  <>
+   <TooltipProvider>
+    <Tooltip delayDuration={0}>
+     <TooltipTrigger asChild={asChild}>{children}</TooltipTrigger>
+    </Tooltip>
+   </TooltipProvider>
+  </>
+ );
+};
