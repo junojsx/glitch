1adafc73bc7d6d6669fcdbb94555badae4f9b4b1
justin-aquino <justin.aquino25@gmail.com>
2024-03-12 23:09:02 -0700

    recommended users frontend component initialized

---
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useSidebar } from "@/store/use-sidebar";
+import { User } from "@prisma/client";
+
+interface RecommendedProps {
+ data: User[];
+}
+
+export const Recommended = ({ data }: RecommendedProps) => {
+ const { collapsed } = useSidebar((state) => state);
+
+ const showLabel = !collapsed && data.length > 0;
+ return (
+  <>
+   <div className="">
+    {showLabel && (
+     <div className="pl-6 mb-4">
+      <p className="text-sm text-muted-foreground"> Recommended</p>
+     </div>
+    )}
+   </div>
+  </>
+ );
+};
