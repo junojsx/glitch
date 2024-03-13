422570bf8b2358c5cb752bd4cbe19fce680555a2
justin-aquino <justin.aquino25@gmail.com>
2024-03-12 22:39:27 -0700

    initializing container wrapping component

---
+++ app/(browse)/_components/container.tsx
@@ -0,0 +1,19 @@
+import { useSidebar } from "@/store/use-sidebar";
+import { cn } from "@/lib/utils";
+
+interface ContainerProps {
+ children: React.ReactNode;
+}
+
+export const Container = ({ children }: ContainerProps) => {
+ const { collapsed } = useSidebar((state) => state);
+ return (
+  <>
+   <div
+    className={cn("flex-1", collapsed ? "ml-[70px]" : "ml-[70px] lg:ml-60px")}
+   >
+    {children}
+   </div>
+  </>
+ );
+};
