230dbe0a9e9601980935f917f6c026c5a1d0d298
justin-aquino <justin.aquino25@gmail.com>
2024-03-13 17:29:41 -0700

    adding live bad on sidebar

--- components/ui/user-avatar.tsx
+++ components/ui/user-avatar.tsx
@@ -2,6 +2,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 import { Skeleton } from "./skeleton";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LiveBadge } from "@/components/ui/live-badge";
 
 const avatarSizes = cva("", {
  variants: {
@@ -47,8 +48,8 @@ export const UserAvatar = ({
     </Avatar>
 
     {canShowBadge && (
-     <div className="absolute -bottom-3 left-1/2 transform-translate-x-1/2">
-      Live
+     <div className="absolute-bottom-3 left-1/2 transform-translate-x-1/2">
+      <LiveBadge />
      </div>
     )}
    </div>
