e165276165f583412c6a7ad29e08011bdb6aa161
justin-aquino <justin.aquino25@gmail.com>
2024-03-17 22:05:41 -0700

    adding notes with the width that causes problems with the sidebar collapsing

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
+import { useState, useEffect } from "react";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
 
 //client component
 //going to have access to hooks
@@ -9,7 +12,23 @@ interface WrapperProps {
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
+ const [isClient, setIsClient] = useState(false);
  const { collapsed } = useSidebar((state) => state);
+
+ useEffect(() => {
+  setIsClient(true);
+ }, []);
+
+ if (!isClient)
+  return (
+   <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+    {" "}
+    {/* the w-70px causes problem with expanding the sidebar */}
+    <ToggleSkeleton />
+    <RecommendedSkeleton />
+   </aside>
+  );
+
  return (
   <>
    <aside
