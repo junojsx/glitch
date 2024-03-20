3d152a4f58b0ea10006ace28a9b0f8ea71ae6007
justin-aquino <justin.aquino25@gmail.com>
2024-03-20 16:18:01 -0700

    initializing buttons Actions component

---
+++ app/(browse)/[username]/_components/actions.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Button } from "@/components/ui/button";
+
+export const Actions = () => {
+ return (
+  <>
+   <div className="">
+    <Button variant="primary">Following</Button>
+   </div>
+  </>
+ );
+};
--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,6 +1,7 @@
 import { isFollowingUser } from "@/lib/follow-service";
 import { getUserByUsername } from "@/lib/user-service";
 import { notFound } from "next/navigation";
+import { Actions } from "./_components/actions";
 
 interface UserPageProps {
  params: {
@@ -22,6 +23,7 @@ const UserPage = async ({ params }: UserPageProps) => {
     <p className="">User: {user.username}</p>
     <p className="">UserId: {user.id}</p>
     <p>is following: {`${isFollowing}`}</p>
+    <Actions />
    </div>
   </>
  );
