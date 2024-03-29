c4712286bbec7d01c40f2bcc56d79dd9fe604533
justin-aquino <justin.aquino25@gmail.com>
2024-03-28 20:58:05 -0700

    adding block function

--- lib/block-service.ts
+++ lib/block-service.ts
@@ -25,3 +25,42 @@ export const isBlockedByUser = async (id: string) => {
   return false;
  }
 };
+
+export const blockUser = async (id: string) => {
+ const self = await getSelf();
+
+ if (self.id === id) {
+  throw new Error("Cannot block yourself!");
+ }
+
+ const otherUser = await db.user.findUnique({
+  where: { id },
+ });
+
+ if (!otherUser) {
+  throw new Error("User not found!");
+ }
+
+ const existingBlock = await db.block.findUnique({
+  where: {
+   blockerId_blockedId: {
+    blockerId: self.id,
+    blockedId: otherUser.id,
+   },
+  },
+ });
+
+ if (existingBlock) {
+  throw new Error("User is already blocked!");
+ }
+
+ const block = await db.block.create({
+  data: {
+   blockerId: self.id,
+   blockedId: otherUser.id,
+  },
+  include: {
+   blocked: true,
+  },
+ });
+};
