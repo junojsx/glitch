a67c4619dc2b1991a6eea0e6b9d8e269d0798a05
justin-aquino <justin.aquino25@gmail.com>
2024-04-09 16:00:44 -0700

    adding onBlock function

--- actions/block.ts
+++ actions/block.ts
@@ -1,5 +1,12 @@
 import { blockUser } from "@/lib/block-service";
+import { revalidatePath } from "next/cache";
 
 export const onBlock = async (id: string) => {
  const blockedUser = await blockUser(id);
+
+ revalidatePath("/");
+
+ if (blockedUser) {
+  revalidatePath(`/${blockedUser.blocked.username}`);
+ }
 };
