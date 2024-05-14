5e9f3d69d08875aa8910780755cd2abafac8652f
justin-aquino <justin.aquino25@gmail.com>
2024-05-13 21:12:59 -0700

    displaying headings for creator dashboard sidebar

--- app/(dashboard)/u/[username]/_components/sidebar/navigation.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/navigation.tsx
@@ -4,9 +4,40 @@ import { usePathname } from "next/navigation";
 import { Fullscreen, KeyRound, MessageSquare, Users } from "lucide-react";
 
 export const Navigation = () => {
+ const pathname = usePathname();
+ const { user } = useUser();
+ const routes = [
+  {
+   label: "Stream",
+   href: `/u/${user?.username}`,
+   icon: Fullscreen,
+  },
+  {
+   label: "Keys",
+   href: `/u/${user?.username}/keys`,
+   icon: KeyRound,
+  },
+  {
+   label: "Chat",
+   href: `/u/${user?.username}/chat`,
+   icon: MessageSquare,
+  },
+  {
+   label: "Community",
+   href: `/u/${user?.username}/community`,
+   icon: Users,
+  },
+ ];
+
  return (
-  <>
-   <nav className="">Navigation</nav>
-  </>
+  <nav aria-label="User Dashboard Side Navigation">
+   <ul className="">
+    {routes.map((route) => (
+     <li className="" key={route.href}>
+      {route.label}
+     </li>
+    ))}
+   </ul>
+  </nav>
  );
 };
