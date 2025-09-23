a84e2b3f3a805d47afa8b98862b60d59eca6f68e
junojsx <junojsx@gmail.com>
2025-09-22 18:41:41 -0700

    test

--- app/(dashboard)/u/[username]/container.tsx
+++ app/(dashboard)/u/[username]/container.tsx
@@ -29,7 +29,7 @@ export const Container = ({ children }: ContainerProps) => {
     className={cn("flex-1", collapsed ? "ml-[70px]" : "ml-[70px] lg:ml-60")}
    >
     {" "}
-    {children}
+    {children}f
    </div>
   </>
  );
